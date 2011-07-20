a56dcf6f0cf2e19cc03170e41caf63f044333456
Erik Hugne <erik.hugne@ericsson.com>
2011-07-20 10:38:28 +0200

    added utility function to insert dstopt in a msg

--- sockopt.c
+++ sockopt.c
@@ -40,7 +40,7 @@ void* get_ancillary_attr(struct msghdr* msg, int cmsg_lvl, int cmsg_type)
 #define CMSG_SET_IPV6_HOPLIMIT(msg, hoplim) set_ancillary_attr(msg, IPPROTO_IPV6, IPV6_HOPLIMIT, &hoplim, sizeof(uint32_t))
 #define CMSG_SET_IPV6_DSCP(msg, dscp)       set_ancillary_attr(msg, IPPROTO_IPV6, IPV6_TCLASS, &dscp, sizeof(uint32_t))
 
-void set_ancillary_attr(struct msghdr* msg, uint32_t cmsg_lvl,
+uint32_t set_ancillary_attr(struct msghdr* msg, uint32_t cmsg_lvl,
                 uint32_t cmsg_type, void* cmsg_data, uint32_t cmsg_len)
 {
     struct cmsghdr *cmsg;
@@ -60,5 +60,22 @@ void set_ancillary_attr(struct msghdr* msg, uint32_t cmsg_lvl,
     cmsg->cmsg_len = CMSG_LEN(cmsg_len);
     value_p = (void*)CMSG_DATA(cmsg);
     memcpy(value_p, cmsg_data, cmsg_len);
+	return cmsg->cmsg_len;
+}
+
+
+
+int create_dstopt(struct msghdr *msg, void *hdrbuf, uint32_t hdrbuf_len, 
+		uint32_t dstopt_len, void** optbuf)
+{
+	uint32_t acclen = 0;
+	uint32_t opt_len = 0;
+	uint32_t offset = 0;
+	offset = inet6_opt_init(NULL,0);
+	opt_len = inet6_opt_append(hdrbuf,hdrbuf_len, offset, 
+			IPV6_DSTOPTS, dstopt_len, 8, optbuf);
+	opt_len = inet6_opt_finish(hdrbuf, hdrbuf_len, dstopt_len);
+	acclen = set_ancillary_attr(msg, IPPROTO_IPV6, IPV6_DSTOPTS, hdrbuf, opt_len);
+	return acclen;
 }
 
--- sockopt.h
+++ sockopt.h
@@ -29,4 +29,29 @@ void set_ancillary_attr(struct msghdr* msg, uint32_t cmsg_lvl,
 #define CMSG_SET_IPV6_HOPLIMIT(msg, hoplim) set_ancillary_attr(msg, IPPROTO_IPV6, IPV6_HOPLIMIT, &hoplim, sizeof(uint32_t))
 #define CMSG_SET_IPV6_DSCP(msg, dscp)       set_ancillary_attr(msg, IPPROTO_IPV6, IPV6_TCLASS, &dscp, sizeof(uint32_t))
 
+/* create_dstopt()
+ * @param msg (struct msghdr*) msghdr to insert the dstopt as controldata in 
+ * @param hdrbuf (void*) data buffer to hold the dstopt 
+ * @param hdrbuf_len (uint32_t)	length of the data buffer
+ * @param dstopt_len (uint32_t) desired length of the dest opt header
+ * @param optbuf (void**) returned pointer to the option data
+ *
+ * @brief Example usage: 
+ * 
+ *	#define BUFLEN 32
+ *	#define WANTED_DSTOPT_LEN 8
+ *	uint32_t acclen = 0;
+ *	char hbuf[BUFLEN];
+ *	memset(hbuf,0,BUFLEN);
+ *	void* optbuf = NULL;
+ *
+ *	acclen += create_dstopt(&msg, hbuf, BUFLEN, 
+ *		 WANTED_DSTOPT_LEN,  &optbuf);
+ *
+ *	@note Remember to update the msg_controllen in the msghdr 
+ *	      to the accumulated length of all options.
+ *		   
+ * */
+int create_dstopt(struct msghdr *msg, void *hdrbuf, uint32_t hdrbuf_len, 
+		uint32_t dstopt_len, void** optbuf);
 
